7dc0224dc1ed6089164f2a1c89e45c9f2d6691f0
VladimirIvanov <ivanovvladimir311992@gmail.com>
2022-10-01 16:56:48 +0300

    Task25 completed added

--- Task25/Program.cs
+++ Task25/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine($"\nЗадача 25. Возведене числа A в натуральную степень B");
+
+int Exponentiation(int numA, int numB){
+  int result = 1;
+  for(int i=1; i <= numB; i++){
+    result = result * numA;
+  }
+    return result;
+}
+
+int ReadData(string msg)
+{
+    Console.Write(msg);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+int numberA = ReadData("Введите число А: ");
+int numberB = ReadData("Введите число В: ");
+int exponentiation = Exponentiation(numberA,numberB);
+
+Console.WriteLine($"{numberA} в  степени {numberB} равно: {exponentiation}");
