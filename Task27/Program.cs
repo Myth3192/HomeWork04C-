963ac689fb2a96d62f3d342cb953b57041676c1d
VladimirIvanov <ivanovvladimir311992@gmail.com>
2022-10-01 21:14:44 +0300

    Task27 added

---
+++ Task27/Program.cs
@@ -0,0 +1,24 @@
+﻿int ReadData(string msg)
+{
+    Console.Write(msg);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+int num = ReadData("Введите число ");
+
+
+int SummNumber(int num)
+{   
+    int count = Convert.ToString(num).Length;
+    int summ = 0;
+    for (int i=1; i <=count; i++)
+    {
+        summ = summ +(num%10);
+        num = num/10;
+    }
+   return summ;
+}
+
+int summ = SummNumber(num);
+Console.WriteLine($"Сумма всех чисел = {summ}");
