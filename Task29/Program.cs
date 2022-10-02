4438ec5e979743b913791fa488c7411f21c11852
VladimirIvanov <ivanovvladimir311992@gmail.com>
2022-10-02 15:37:29 +0300

    Task29 added

---
+++ Task29/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите элементы массива");
+string []array = new string[8];
+for (int i=0; i<8; i++)
+{   
+    string number =Console.ReadLine();
+    array[i] = number;
+}
+
+array = array.Where(x => !string.IsNullOrWhiteSpace(x)).ToArray();
+Console.WriteLine("[{0}]", string.Join(", ", array));
